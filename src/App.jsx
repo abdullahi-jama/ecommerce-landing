e2cb5284c7245e1d80adb112e6e3864d1719ae6d
Abdullahi jama <abdullahi.jama122@gmail.com>
2025-06-10 22:54:50 +0200

    landing page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+     
+      <Navbar />
+      <Hero />
+      <Products />
+      <CTA />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
